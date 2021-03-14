946f8d9b918d749efa1bda2dc9ed9222fa26b78f
Ragil Burhanudin Pamungkas <om.ragil7@gmail.com>
2021-03-14 22:27:53 +0700

    feat: add responsive mode and button for message in dashboard

---
+++ assets/js/custom.js
@@ -0,0 +1,47 @@
+if (document.readyState === "complete") {
+  // toggle menu mobile
+}
+
+const showMenu = document.getElementById("humberger-icon");
+showMenu.addEventListener("click", function () {
+  const sidebar = document.getElementById("sidebar");
+
+  if (sidebar.style.display === "flex") {
+    sidebar.style.display = "none";
+    if (document.getElementById("main-content")) {
+      document.getElementById("main-content").style = "flex";
+    }
+    if (document.getElementById("dashboard-peserta")) {
+      document.getElementById("dashboard-peserta").style = "flex";
+    }
+  } else {
+    sidebar.style.display = "flex";
+    if (document.getElementById("main-content")) {
+      document.getElementById("main-content").style = "none";
+    }
+    if (document.getElementById("dashboard-peserta")) {
+      document.getElementById("dashboard-peserta").style = "none";
+    }
+  }
+  if (document.getElementById("message-container")) {
+    document.getElementById("message-container").style = "none";
+  }
+});
+
+if (document.getElementById("chat-icon")) {
+  document.getElementById("chat-icon").addEventListener("click", function () {
+    const messageContainer = document.getElementById("message-container");
+    if (messageContainer.style.display === "flex") {
+      messageContainer.style.display = "none";
+      if (document.getElementById("dashboard-peserta")) {
+        document.getElementById("dashboard-peserta").style.display = "flex";
+      }
+    } else {
+      messageContainer.style.display = "flex";
+      if (document.getElementById("dashboard-peserta")) {
+        document.getElementById("dashboard-peserta").style.display = "none";
+      }
+    }
+    document.getElementById("sidebar").style.display = "none";
+  });
+}
